896b7a16162c3b5d8743c37c3a70acaf1bb91a42
Ricardo <ricardo@42.fr>
2026-04-02 16:42:59 +0200

    norminette

--- include/data_minirt.h
+++ include/data_minirt.h
@@ -6,7 +6,7 @@
 /*   By: ricardo <ricardo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 10:12:59 by rfleritt          #+#    #+#             */
-/*   Updated: 2026/02/07 18:34:28 by ricardo          ###   ########.fr       */
+/*   Updated: 2026/04/02 16:33:05 by ricardo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,134 +15,134 @@
 
 typedef enum s_order
 {
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT,
-    A,
-    D
-}   t_order;
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT,
+	A,
+	D
+}	t_order;
 
 typedef enum s_type_scene
 {
-    SPHERE,
-    PLANE,
-    AMBIENT,
-    CAMERA,
-    LIGHT,
-    CYLINDER,
-    VOID
-}   t_type_scene;
+	SPHERE,
+	PLANE,
+	AMBIENT,
+	CAMERA,
+	LIGHT,
+	CYLINDER,
+	VOID
+}	t_type_scene;
 
 typedef struct s_vec3
 {
-    float x;
-    float y;
-    float z;
-}   t_vec3;
+	float	x;
+	float	y;
+	float	z;
+}	t_vec3;
 
 typedef struct s_camera
 {
-    t_vec3  position;
-    t_vec3  direction;
-    t_vec3 forward;
-    t_vec3 up;
-    t_vec3 right;
-    float   pov;
-}   t_camera;
+	t_vec3	position;
+	t_vec3	direction;
+	t_vec3	forward;
+	t_vec3	up;
+	t_vec3	right;
+	float	pov;
+}	t_camera;
 
 typedef struct s_color
 {
-    float r;
-    float g;
-    float b;
-}   t_color;
+	float	r;
+	float	g;
+	float	b;
+}	t_color;
 
 typedef struct s_plane
 {
-    t_vec3 point;
-    t_vec3 normal;
-    t_color color;
-}   t_plane;
+	t_vec3	point;
+	t_vec3	normal;
+	t_color	color;
+}	t_plane;
 
 typedef struct s_light
 {
-    t_vec3 position;
-    float intensity;
-    t_color color;
-}   t_light;
+	t_vec3	position;
+	float	intensity;
+	t_color	color;
+}	t_light;
 
 typedef struct s_ambient
 {
-    float intensity;
-    t_color color;
-}   t_ambient;
+	float	intensity;
+	t_color	color;
+}	t_ambient;
 
 typedef struct s_ray
 {
-    t_vec3 origin;
-    t_vec3 direction;
-}   t_ray;
+	t_vec3	origin;
+	t_vec3	direction;
+}	t_ray;
 
 typedef struct s_hit_info
 {
-    float t;
-    t_vec3 point;
-    t_vec3 normal;
-    t_color color;
-}   t_hit_info;
+	float	t;
+	t_vec3	point;
+	t_vec3	normal;
+	t_color	color;
+}	t_hit_info;
 
 typedef struct s_sphere
 {
-    t_vec3 center;
-    float radius;
-    t_color color;
-}   t_sphere;
+	t_vec3	center;
+	float	radius;
+	t_color	color;
+}	t_sphere;
 
 typedef struct s_cylinder
 {
-    t_vec3 center;
-    t_vec3 axis;
-    float radius;
-    float height;
-    t_color color;
-} t_cylinder;
+	t_vec3	center;
+	t_vec3	axis;
+	float	radius;
+	float	height;
+	t_color	color;
+}	t_cylinder;
 
 typedef struct s_window
 {
-    mlx_t *mlx;
-    mlx_image_t *mlx_image;
-	int width;
-    int height;
+	mlx_t		*mlx;
+	mlx_image_t	*mlx_image;
+	int			width;
+	int			height;
 }	t_window;
 
 typedef struct s_scene
 {
-    t_type_scene type;
-    char **data;
-    struct s_scene *next;
-}   t_scene;
+	t_type_scene	type;
+	char			**data;
+	struct s_scene	*next;
+}	t_scene;
 
 typedef struct s_scene_data
 {
-    t_camera camera;
-    t_sphere *sphere;
-    t_ambient ambient;
-    t_plane *plane;
-    t_light *light;
-    t_cylinder *cylinder;
-    int n_cylinder;
-    int n_sphere;
-    int n_plane;
-    int n_light;
-}   t_scene_data;
+	t_camera	camera;
+	t_sphere	*sphere;
+	t_ambient	ambient;
+	t_plane		*plane;
+	t_light		*light;
+	t_cylinder	*cylinder;
+	int			n_cylinder;
+	int			n_sphere;
+	int			n_plane;
+	int			n_light;
+}	t_scene_data;
 
 typedef struct s_data
 {
-    t_window *window;
-    t_scene_data *scene;
-    t_ray ray;
-    t_scene *token;
-}   t_data;
+	t_window		*window;
+	t_scene_data	*scene;
+	t_ray			ray;
+	t_scene			*token;
+}	t_data;
 
 #endif
--- include/minirt.h
+++ include/minirt.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minirt.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rfleritt <rfleritt@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ricardo <ricardo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 08:36:35 by rfleritt          #+#    #+#             */
-/*   Updated: 2026/03/12 17:41:46 by rfleritt         ###   ########.fr       */
+/*   Updated: 2026/04/02 16:40:41 by ricardo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,63 +25,62 @@
 # define HEIGHT 600
 # define M_PI 3.14159265358979323846
 
-void init_window(t_data *data);
-void key_close_window(mlx_key_data_t keydata, void *param);
-void ft_render(t_data *data);
-void exit_msg_error(char *err);
-int hit_sphere_info(t_ray ray, t_sphere sphere, t_hit_info *hit);
-float hit_plane(t_ray ray, t_plane plane);
-int hit_plane_info(t_ray ray, t_plane plane, t_hit_info *hit);
-int ft_msg_error(char *err, int ret);
-t_data *init_scene(char *scene);
-float vec3_dot(t_vec3 a, t_vec3 b);
-float vec_length(t_vec3 v);
-t_vec3 vec_new(float x, float y, float z);
-t_vec3 vec_mult(t_vec3 v, float t);
-t_vec3 vec_sub(t_vec3 a, t_vec3 b);
-t_vec3 vec_add(t_vec3 a, t_vec3 b);
-t_vec3 vec_norm(t_vec3 v);
-t_vec3 vec_cross(t_vec3 a, t_vec3 b);
-t_vec3 parse_vec(char *vec);
-void init_hit_sphere(int x, int y, t_data *data);
-void init_camera(t_data *data);
-void key_register(mlx_key_data_t keydata, void *param);
-float hit_sphere(t_ray ray, t_sphere sphere);
-void parse_scene(t_data *data);
-void parse_sphere(t_data *data);
-void parse_ambient(t_data *data);
-void parse_light(t_data *data);
-void parse_plane(t_data *data);
-void parse_camera(t_data *data);
-t_sphere set_sphere(char **data);
-t_light set_light(char **data);
-t_plane set_plane(char **data);
-t_camera set_camera(char **data);
-t_ambient set_ambient(char **data);
-t_color parse_color(char *color);
-t_color calculate_ambient(t_ambient ambient, t_color object_color);
-t_color calculate_diffuse(t_light light, t_hit_info hit);
-t_color add_colors(t_color c1, t_color c2);
-int is_in_shadow(t_data *data, t_hit_info hit, t_light light);
-t_color calculate_specular(t_light light, t_hit_info hit, t_vec3 view_dir);
-t_color calculate_lighting(t_data *data, t_hit_info hit, t_vec3 view_dir);
-void free_tokens(t_scene *token);
-void free_scene(t_scene_data *scene);
-void cleanup_data(t_data *data);
-int found_count_element(t_scene *scene, t_type_scene type);
-t_cylinder set_cylinder(char **data);
-void parse_cylinder(t_data *data);
-int hit_cylinder_info(t_ray ray, t_cylinder cy, t_hit_info *hit);
-void	ft_render_image(t_color color, int x, int y, t_window *window);
-t_color	lerp_color(t_color c1, t_color c2, float t);
-double	lerp(float a, float b, float t);
-int	check_spheres(t_data *data, t_ray ray, t_hit_info *hit,
-	float *closest_t);
-int	cylinder_body_hit(t_ray ray, t_cylinder cy, t_hit_info *hit);
-int	hit_cylinder_info(t_ray ray, t_cylinder cy, t_hit_info *hit);
-int	hit_cylinder_caps(t_ray ray, t_cylinder cy, t_hit_info *hit);
-float	cylinder_disc(t_ray ray, t_cylinder cy, float *a, float *b);
-int	fill_cyl(t_ray ray, t_cylinder cy, float t, t_hit_info *hit);
-
+void		init_window(t_data *data);
+void		key_close_window(mlx_key_data_t keydata, void *param);
+void		ft_render(t_data *data);
+void		exit_msg_error(char *err);
+int			hit_sphere_info(t_ray ray, t_sphere sphere, t_hit_info *hit);
+float		hit_plane(t_ray ray, t_plane plane);
+int			hit_plane_info(t_ray ray, t_plane plane, t_hit_info *hit);
+int			ft_msg_error(char *err, int ret);
+t_data		*init_scene(char *scene);
+float		vec3_dot(t_vec3 a, t_vec3 b);
+float		vec_length(t_vec3 v);
+t_vec3		vec_new(float x, float y, float z);
+t_vec3		vec_mult(t_vec3 v, float t);
+t_vec3		vec_sub(t_vec3 a, t_vec3 b);
+t_vec3		vec_add(t_vec3 a, t_vec3 b);
+t_vec3		vec_norm(t_vec3 v);
+t_vec3		vec_cross(t_vec3 a, t_vec3 b);
+t_vec3		parse_vec(char *vec);
+void		init_hit_sphere(int x, int y, t_data *data);
+void		init_camera(t_data *data);
+void		key_register(mlx_key_data_t keydata, void *param);
+float		hit_sphere(t_ray ray, t_sphere sphere);
+void		parse_scene(t_data *data);
+void		parse_sphere(t_data *data);
+void		parse_ambient(t_data *data);
+void		parse_light(t_data *data);
+void		parse_plane(t_data *data);
+void		parse_camera(t_data *data);
+t_sphere	set_sphere(char **data);
+t_light		set_light(char **data);
+t_plane		set_plane(char **data);
+t_camera	set_camera(char **data);
+t_ambient	set_ambient(char **data);
+t_color		parse_color(char *color);
+t_color		calculate_ambient(t_ambient ambient, t_color object_color);
+t_color		calculate_diffuse(t_light light, t_hit_info hit);
+t_color		add_colors(t_color c1, t_color c2);
+int			is_in_shadow(t_data *data, t_hit_info hit, t_light light);
+t_color		calculate_specular(t_light light, t_hit_info hit, t_vec3 view_dir);
+t_color		calculate_lighting(t_data *data, t_hit_info hit, t_vec3 view_dir);
+void		free_tokens(t_scene *token);
+void		free_scene(t_scene_data *scene);
+void		cleanup_data(t_data *data);
+int			found_count_element(t_scene *scene, t_type_scene type);
+t_cylinder	set_cylinder(char **data);
+void		parse_cylinder(t_data *data);
+int			hit_cylinder_info(t_ray ray, t_cylinder cy, t_hit_info *hit);
+void		ft_render_image(t_color color, int x, int y, t_window *window);
+t_color		lerp_color(t_color c1, t_color c2, float t);
+double		lerp(float a, float b, float t);
+int			check_spheres(t_data *data, t_ray ray, t_hit_info *hit,
+				float *closest_t);
+int			cylinder_body_hit(t_ray ray, t_cylinder cy, t_hit_info *hit);
+int			hit_cylinder_info(t_ray ray, t_cylinder cy, t_hit_info *hit);
+int			hit_cylinder_caps(t_ray ray, t_cylinder cy, t_hit_info *hit);
+float		cylinder_disc(t_ray ray, t_cylinder cy, float *a, float *b);
+int			fill_cyl(t_ray ray, t_cylinder cy, float t, t_hit_info *hit);
 
 #endif
